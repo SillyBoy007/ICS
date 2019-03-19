ced09528b5b5f532d6c022a55d4f23366e77d353
wangxiayun <wangxiayun@aikucun.com>
2019-03-19 21:00:08 +0800

    增加slf4j日志

--- ics-member/ics-member-service/src/main/java/com/wxy/ics/member/controller/MemberController.java
+++ ics-member/ics-member-service/src/main/java/com/wxy/ics/member/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.wxy.ics.member.controller;
 
+import com.wxy.ics.common.enums.CodeMsg;
 import com.wxy.ics.member.common.controller.BaseController;
 import com.wxy.ics.member.domain.Member;
 import com.wxy.ics.member.service.MemberService;
 import entity.ReturnResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,17 +17,29 @@ import static com.wxy.ics.common.enums.CodeMsg.*;
  */
 @RestController
 @RequestMapping("/api/member")
+@Slf4j
 public class MemberController extends BaseController {
     @Autowired
     MemberService memberService;
     @RequestMapping(value = "/get/{id}",method = RequestMethod.GET)
     public ReturnResult<Member> getMemberById(@PathVariable("id") Long id){
-        ReturnResult<Member> returnResult = new ReturnResult<>();
-        returnResult.setCode(SUCCESS.getKey());
-        returnResult.setData(memberService.selectByKey(id));
-        returnResult.setMsg(SUCCESS.getMessage());
-        returnResult.setSuccess(true);
-        return returnResult;
+        ReturnResult<Member> returnResult;
+        try {
+            returnResult = new ReturnResult<>();
+            returnResult.setCode(SUCCESS.getKey());
+            returnResult.setData(memberService.selectByKey(id));
+            returnResult.setMsg(SUCCESS.getMessage());
+            returnResult.setSuccess(true);
+            return returnResult;
+        }catch (Exception e){
+            returnResult = new ReturnResult<>();
+            returnResult.setCode(SERVER_ERROR.getKey());
+            returnResult.setMsg(SERVER_ERROR.getMessage());
+            returnResult.setSuccess(false);
+            log.error("查询id为{}的用户异常",id,e);
+            return returnResult;
+        }
+
     }
 
 }
